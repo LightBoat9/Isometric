1f244e692b43020f7c417f953e1bcf26cfff1148
Luke <gamersloop@gmail.com>
2017-11-09 15:54:53 -0500

    More Refactor

--- NPCs/NPC.gd
+++ NPCs/NPC.gd
@@ -2,7 +2,6 @@ extends "res://References/NodeReference.gd"
 
 # NPC Nodes
 onready var MouseArea = get_node("MouseArea")
-onready var NPCHighlight = get_node("NPCHighlight")
 onready var NPCSprite = get_node("NPCSprite")
 onready var NPCDialogue = get_node("NPCDialogue")
 
@@ -21,19 +20,28 @@ func _ready():
 	set_process_input(true)
 	
 func _input(event):
-	if (event.is_action_pressed("mouse_left")):
+	if (event.type == InputEvent.MOUSE_MOTION):
+		if (_mouse_is_over()):
+			mouse_over = true
+			NPCSprite.get_material().set_shader_param("visible", 1)
+		else:
+			NPCSprite.get_material().set_shader_param("visible", 0)
+			mouse_over = false
+	elif (event.is_action_pressed("mouse_left")):
 		if (mouse_over):
 			if (text_box == null):
 				Player.StateMachine.current_state = "menu"
 				spawn_text_box()
-	
-func mouse_enter():
-	mouse_over = true
-	NPCHighlight.show()
-	
-func mouse_exit():
-	mouse_over = false
-	NPCHighlight.hide()
+			
+func _mouse_is_over():
+	var h = NPCSprite.get_texture().get_height()
+	var w = NPCSprite.get_texture().get_width()
+	return (
+		NPCSprite.get_global_mouse_pos().x - get_global_pos().x < w && 
+		NPCSprite.get_global_mouse_pos().x - get_global_pos().x > 0 &&
+		NPCSprite.get_global_mouse_pos().y - get_global_pos().y < h && 
+		NPCSprite.get_global_mouse_pos().y - get_global_pos().y > 0
+		)
 	
 func spawn_text_box():
 	text_box = TextBox.instance()
